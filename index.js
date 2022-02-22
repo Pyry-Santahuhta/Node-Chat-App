e913319d6fa655975c9268bbe717ece44e617b67
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2022-02-22 18:24:12 +0200

    Final commit

--- index.js
+++ index.js
@@ -6,39 +6,48 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server);
 
+//Serve client side as a static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//Respond with index.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+//Used for emitting to only clients with the same channel
 let thisChannel = "";
+//Storing users
 let users = [];
 
+//On TCP connection
 io.on("connection", (socket) => {
   console.log("a user connected");
-
+  //Join channel happens when a user sets their username
   socket.on("join channel", (data) => {
+    //Add a new user
     let newUser = addUser(socket.id, data.username, data.channel);
-    socket.emit("send data", {
+
+    //Send the user their id
+    socket.emit("send id", {
       id: socket.id,
-      username: data.username,
-      channel: data.channel,
     });
-    thisChannel = data.channel;
+    thisChannel = newUser.channel;
     socket.join(newUser.channel);
   });
 
+  //Emit chat message to all users in the channel
   socket.on("chat message", (msg) => {
     io.to(thisChannel).emit("chat message", msg);
   });
 
+  //Remove user when disconnected
   socket.on("disconnect", () => {
     removeUser(socket.id);
     console.log("user disconnected");
   });
 });
 
+//Function for adding user to the user list
 function addUser(socketID, userName, channelID) {
   const user = {
     socket: socketID,
@@ -49,6 +58,7 @@ function addUser(socketID, userName, channelID) {
   return user;
 }
 
+//Function for removing users from the user list
 function removeUser(socketID) {
   const user = (users) => users.socket === socketID;
   const index = users.findIndex(user);
@@ -57,6 +67,7 @@ function removeUser(socketID) {
   }
 }
 
+//Start the server
 server.listen(3000, () => {
   console.log("listening on port:3000");
 });
--- public/client.js
+++ public/client.js
@@ -3,31 +3,44 @@ const socket = io();
 const input = document.getElementById("input");
 const channelInfo = document.getElementById("channel-info");
 let username = "";
+//On load ask for the channel the user wants to join
 let channel = prompt("Give the channel name you wish to join");
 let id = "";
 
-socket.on("send data", (data) => {
+//Get the user id back on joining channel
+socket.on("send id", (data) => {
   id = data.id;
 });
 
+//Listener for the text bar
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
+    //If there is no username, set the username
     if (username === "") {
+      //Default channel in case of user canceling the prompt
+      if (channel === null) {
+        channel = 1;
+      }
+
       username = input.value;
+      //Change the input prompt and channel info
       input.placeholder = "Write a message as " + username;
       channelInfo.innerHTML = "You are in channel " + channel;
 
+      //Send join channel info to server
       socket.emit("join channel", { username: username, channel: channel });
     } else {
+      //The user already has a username, so just send a message
       const message = {
         text: input.value,
         name: username,
         id: id,
       };
+      //Emit the message
       socket.emit("chat message", message);
     }
-
+    //Empty the input bar
     input.value = "";
   }
 });
@@ -35,7 +48,7 @@ form.addEventListener("submit", function (e) {
 socket.on("chat message", function (msg) {
   const newMessage = document.createElement("li");
 
-  //Get current time, add leading 0's to time's so they look nice.
+  //Get current time, add leading 0's to times so they look nice.
   const now = new Date();
   let HH = now.getHours();
   let MM = now.getMinutes();
@@ -46,11 +59,14 @@ socket.on("chat message", function (msg) {
     MM = "0" + MM;
   }
   const current = HH + ":" + MM;
+
+  //If the message's id is the user's own, use the ownMessage CSS, otherwise use receivedMessage
   if (msg.id === id) {
     newMessage.className = "ownMessage";
   } else {
     newMessage.className = "receivedMessage";
   }
+  //Set the text for a new message
   newMessage.innerHTML = msg.name + ": " + msg.text + "<br />" + current;
   messages.appendChild(newMessage);
   window.scrollTo(0, document.body.scrollHeight);
